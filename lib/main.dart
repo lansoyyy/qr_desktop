544690244ecd86b4976f0d8c2fce61f58441d18b
Lans12345 <olanalans12345@gmail.com>
2024-02-10 08:55:04 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_scanner/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyB0HijyKZx-5rqr8Ryp4HKHtZsflpoX0_o",
+          projectId: "amusement-2e6a5",
+          storageBucket: "amusement-2e6a5.appspot.com",
+          messagingSenderId: "928127195723",
+          appId: "1:928127195723:web:06834331060942c8ec0bce"));
   runApp(const MyApp());
 }
 
